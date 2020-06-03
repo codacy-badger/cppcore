cbb01b6f6bf3ccc0061c41e7e33cba3f2c2acf00
raeperd <raeperd117@gmail.com>
2020-06-03 23:10:29 +0900

    Add exception_format

---
+++ Src/000_String/Exception.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "Exception.h"
+#include "STLString.h"
+#include "CStrLegacy.h"
+#include "Unicode.h"
+
+namespace core
+{
+	//////////////////////////////////////////////////////////////////////////
+	exception_format::exception_format(const TCHAR* pszFormat, ...)
+		: std::exception()
+	{
+		const size_t tBuffSize = 2048;
+		TCHAR szBuff[tBuffSize] = { 0, };
+		va_list list;
+		va_start(list, pszFormat);
+		SafeSVPrintf(szBuff, tBuffSize, pszFormat, list);
+		va_end(list);
+
+		m_strWhatA = MBSFromTCS(szBuff);
+		m_strWhatW = WCSFromTCS(szBuff);
+	}
+}
+
